bec6a3a7441a6518da5a3f54c4042a6de5c6dedc
kabboCSE <shahriar15-10494@diu.edu.bd>
2026-06-11 02:03:05 +0600

    update Player component to use 'object-cover' for fullscreen video rendering

--- src/App.jsx
+++ src/App.jsx
@@ -352,7 +352,7 @@ function Player({
         >
           <video
             ref={videoRef}
-            className={`w-full h-full ${isFullscreen ? "object-fill" : "object-contain"}`}
+            className={`w-full h-full ${isFullscreen ? "object-cover" : "object-contain"}`}
             playsInline
             autoPlay
             crossOrigin="anonymous"
